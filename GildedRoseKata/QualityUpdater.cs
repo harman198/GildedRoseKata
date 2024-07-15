0467a13513adf7235462585a2f89f27816749212
Harman Khangura <hakh@collectionhouse.com.au>
2024-07-15 23:57:22 +1000

    Inlined DoStuff2 method

--- GildedRoseKata/QualityUpdater.cs
+++ GildedRoseKata/QualityUpdater.cs
@@ -20,61 +20,68 @@ public class QualityUpdater
         }
         if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
         {
-            DoStuff2(item);
-            return;
-        }
-        else
-        {
-            DoStuff2(item);
-            return;
-        }
-    }
-
-    private static void DoStuff2(Item item)
-    {
-        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-        {
-            if (item.Quality > 0)
+            if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                if (item.Quality > 0)
                 {
-                    item.Quality = item.Quality - 1;
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        item.Quality = item.Quality - 1;
+                    }
                 }
             }
-        }
-        else
-        {
-            if (item.Quality < 50)
+            else
             {
-                item.Quality = item.Quality + 1;
-
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Quality < 50)
                 {
-                    if (item.SellIn < 11)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
+                    item.Quality = item.Quality + 1;
 
-                    if (item.SellIn < 6)
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (item.Quality < 50)
+                        if (item.SellIn < 11)
                         {
-                            item.Quality = item.Quality + 1;
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
                         }
                     }
                 }
             }
-        }
 
-        if (item.Name != "Sulfuras, Hand of Ragnaros")
-        {
-            item.SellIn = item.SellIn - 1;
-        }
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn = item.SellIn - 1;
+            }
 
-        if (item.SellIn < 0)
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    item.Quality = item.Quality - item.Quality;
+                }
+            }
+            return;
+        }
+        else
         {
             if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
@@ -88,8 +95,54 @@ public class QualityUpdater
             }
             else
             {
-                item.Quality = item.Quality - item.Quality;
+                if (item.Quality < 50)
+                {
+                    item.Quality = item.Quality + 1;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (item.SellIn < 11)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
+                            {
+                                item.Quality = item.Quality + 1;
+                            }
+                        }
+                    }
+                }
             }
+
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn = item.SellIn - 1;
+            }
+
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (item.Quality > 0)
+                    {
+                        if (item.Name != "Sulfuras, Hand of Ragnaros")
+                        {
+                            item.Quality = item.Quality - 1;
+                        }
+                    }
+                }
+                else
+                {
+                    item.Quality = item.Quality - item.Quality;
+                }
+            }
+            return;
         }
     }
 
